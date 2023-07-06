c7dad28a84b1247dd90c9272d63ff1d576429ed7
sumana3181 <138704275+sumana3181@users.noreply.github.com>
2023-07-06 12:06:49 +0000

    git commit

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
   };
   return (
     <Container>
-      <AppLabel>React Weather App</AppLabel>
+      <AppLabel>Quick Weather</AppLabel>
       {city && weather ? (
         <WeatherComponent weather={weather} city={city} />
       ) : (
