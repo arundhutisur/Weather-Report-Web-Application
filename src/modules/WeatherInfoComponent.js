cd3d2ccf61fe91ada9e7dc2ec370e4a73fc79f0d
Arundhuti Sur <85032238+arundhutisur@users.noreply.github.com>
2023-07-09 16:38:54 +0000

    git commit

--- src/modules/WeatherInfoComponent.js
+++ src/modules/WeatherInfoComponent.js
@@ -3,12 +3,12 @@ import styled from "styled-components";
 import {WeatherIcons} from "../App";
 
 export const WeatherInfoIcons = {
-    sunset: "/icons/temp.svg",
-    sunrise: "/icons/temp.svg",
-    humidity: "/icons/humidity.svg",
-    wind: "/icons/wind.svg",
-    min_temp: "/icons/mintemp.svg",
-    max_temp: "/icons/mintemp.svg"
+    sunset: "./icons/temp.svg",
+    sunrise: "./icons/temp.svg",
+    humidity: "./icons/humidity.svg",
+    wind: "./icons/wind.svg",
+    min_temp: "./icons/mintemp.svg",
+    max_temp: "./icons/mintemp.svg"
 };
 const Location = styled.span`
   margin: 15px auto;
